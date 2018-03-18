c7f806c69933cf09ad87c6b18f76f61ebf6ccd77
asem <andriysemchenko@gmail.com>
2018-03-18 19:21:16 +0300

    implemented Object class

--- Object.cpp
+++ Object.cpp
@@ -1,2 +1,10 @@
 #include "stdafx.h"
 #include "Object.h"
+
+Object::Object(Field * f, Owner own): field(f), owner(own) {
+}
+
+Object::Owner Object::getOwner() const noexcept
+{
+	return owner;
+}
--- Object.h
+++ Object.h
@@ -1,12 +1,24 @@
-#pragma once
+#ifndef OBJECT_H
+#define OBJECT_H
+class Field;
 class Object
 {
 public:
-	Object() = default;
+	enum Status { DESTROYED, NOT_DESTROYED };
+	enum Owner { USER, COMPUTER };
+	Object(Field *f, Owner own);
 	virtual void move() = 0;
+	virtual Status event() = 0;
+	virtual void tryDestroy(const Object* destroyer) = 0;
+	virtual void draw() = 0;
+	virtual void hide() = 0;
 	virtual ~Object() = default;
+	Owner getOwner() const noexcept;
 protected:
+	Field * field;
+	Owner owner;
 	int x;
 	int y;
 };
 
+#endif // !OBJECT_H
